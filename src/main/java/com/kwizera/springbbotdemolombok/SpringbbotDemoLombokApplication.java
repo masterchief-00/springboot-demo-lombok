b33c3c06be07c3bcbee248148e62e8d41b1c30d0
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-06-12 16:14:28 +0200

    chore: init commit

---
+++ src/main/java/com/kwizera/springbbotdemolombok/SpringbbotDemoLombokApplication.java
@@ -0,0 +1,13 @@
+package com.kwizera.springbbotdemolombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbbotDemoLombokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbbotDemoLombokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwizera/springbbotdemolombok/SpringbbotDemoLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwizera.springbbotdemolombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbbotDemoLombokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
